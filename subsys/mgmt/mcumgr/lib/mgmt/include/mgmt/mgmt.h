94cc1c150104db0fd332a017cfb2490b4888bf27
Andrew Hedin <andrew.hedin@lairdconnect.com>
2022-04-21 12:58:02 -0500

    Fix spacing
    Signed-off-by: Andrew Hedin <andrew.hedin@lairdconnect.com>

--- subsys/mgmt/mcumgr/lib/mgmt/include/mgmt/mgmt.h
+++ subsys/mgmt/mcumgr/lib/mgmt/include/mgmt/mgmt.h
@@ -51,17 +51,17 @@ extern "C" {
 #define MGMT_ERR_EINVAL			3
 #define MGMT_ERR_ETIMEOUT		4
 #define MGMT_ERR_ENOENT			5
-#define MGMT_ERR_EBADSTATE		6	   /* Current state disallows command. */
-#define MGMT_ERR_EMSGSIZE		7	   /* Too large for transport. */
-#define MGMT_ERR_ENOTSUP		8	   /* Command not supported. */
-#define MGMT_ERR_ECORRUPT		9	   /* Corrupt */
-#define MGMT_ERR_NO_CLIENT		10	   /* Client handler not found */
+#define MGMT_ERR_EBADSTATE		6	/* Current state disallows command. */
+#define MGMT_ERR_EMSGSIZE		7	/* Too large for transport. */
+#define MGMT_ERR_ENOTSUP		8	/* Command not supported. */
+#define MGMT_ERR_ECORRUPT		9	/* Corrupt */
+#define MGMT_ERR_NO_CLIENT		10	/* Client handler not found */
 #define MGMT_ERR_DECODE			11
 #define MGMT_ERR_ENCODE			12
 #define MGMT_ERR_OFFSET			13
 #define MGMT_ERR_TRANSPORT		14
 #define MGMT_ERR_BUSY			15
-#define MGMT_ERR_WRITE			16		/* Can't write CBOR to transport */
+#define MGMT_ERR_WRITE			16	/* Can't write CBOR to transport */
 #define MGMT_ERR_OPEN			17
 #define MGMT_ERR_CLOSE			18
 #define MGMT_ERR_LENGTH_MISSING 19
@@ -82,7 +82,7 @@ extern "C" {
 
 struct mgmt_hdr {
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-	uint8_t  nh_op:3;		   /* MGMT_OP_[...] */
+	uint8_t  nh_op:3;		/* MGMT_OP_[...] */
 	uint8_t  _res1:5;
 #endif
 #if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
@@ -103,7 +103,7 @@ struct mgmt_hdr {
 		.nh_op = (op),					\
 		.nh_flags = 0,					\
 		.nh_len = htons(len),			\
-		.nh_group = htons(group),   	\
+		.nh_group = htons(group),		\
 		.nh_seq = mgmt_get_sequence(),	\
 		.nh_id = (id)					\
 	}
